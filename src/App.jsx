3ecfa21e20656498477c1b7d8cbc01cef4c32f47
jibananando <jibananando.pramanik@gmail.com>
2025-05-06 17:06:45 +0600

    object distractureing + pass value by props

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-400 h-24 w-24 text-lg m-1 leading-9">
+      {value}
+    </button>
+  );
+}
+
+export default function Bord() {
+  return (
+    <>
+      <div>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
+}
